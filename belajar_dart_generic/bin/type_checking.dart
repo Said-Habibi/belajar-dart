1377dacd0f9a8d742473aa1a77767e79ed68b0cb
habibi <elekgaming008@gmail.com>
2026-01-12 22:20:16 +0700

    upload hasil pembelajaran

--- belajar_dart_generic/bin/type_checking.dart
+++ belajar_dart_generic/bin/type_checking.dart
@@ -9,7 +9,7 @@ void main(){
   var intData = Mydata<int>(42);
   var numData = Mydata<num>(3.14);
 
-  check(stringData);  // Output: "object" 😱 Bukan "string"!
-  check(intData);     // Output: "object" 😱 Bukan "num"!
-  check(numData);     // Output: "object" 😱 Bukan "num"!
+  check(stringData);
+  check(intData);
+  check(numData);
 }
